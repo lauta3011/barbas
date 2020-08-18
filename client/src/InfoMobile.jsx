6f180e03dc1772d0911f4c9ea21686c6d640fd8f
Lautaro <lauta_3011@hotmail.com>
2020-08-18 20:12:17 -0300

    first changes to the mobile components

--- client/src/InfoMobile.jsx
+++ client/src/InfoMobile.jsx
@@ -1,16 +1,37 @@
 import React, { Component } from 'react';
 
 class InfoMobile extends Component {
-    state = {  }
+    state = {  
+        checked: false,
+        backgroundColor: 'white',
+        h1Color: 'black',
+        pColor: 'grey'
+    }
     render() { 
         return (  
-            <div className="InfoMobile">
-                <div>
-                    <h3>{this.props.type}</h3>
-                    <p>{this.props.price}</p>
+            <div className="InfoMobile" style={{backgroundColor:this.state.backgroundColor}}>
+                <div onClick={this.checkService} >
+                    <h3 style={{color:this.state.h1Color}}>{this.props.title}</h3>
+                    <p style={{color:this.state.pColor}}>{this.props.desc}</p>
                 </div>
             </div>
         );
+    }
+
+    checkService = () =>{
+        if(this.state.checked === false){
+            this.setState({ checked: true});
+            this.setState({ backgroundColor: 'grey'});
+            this.setState({ h1Color: 'white'});
+            this.setState({ pColor: 'white'});
+            this.props.isChecked();
+        }else{
+            this.setState({ checked: false});
+            this.setState({ backgroundColor: 'white'});
+            this.setState({ h1Color: 'black'});
+            this.setState({ pColor: 'grey'});
+            this.props.isUnchecked();
+        }
     }
 }
  
