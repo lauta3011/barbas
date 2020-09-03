56ae1ca2e941c60b7465ffe4336da7c984c5d204
Lautaro Rodriguez <lauta_3011@hotmail.com>
2020-09-03 01:25:57 -0300

    added a modal to show the reservation info

--- client/src/Servicios.jsx
+++ client/src/Servicios.jsx
@@ -1,27 +1,27 @@
 import React, { Component } from 'react';
 import CrearReserva from './CrearReserva';
 import Model from './Model';
+import ShowReservation from './ShowReservation';
 import axios from 'axios';
 
 class Servicios extends Component {
     state = { 
+        showModal : 'none',
+        name : '',
+        date : '',
+        time : '',
         serviceType : []
      }
 
      
     render() { 
         return ( 
-            <div className="Servicios">
-                <div>
-                    <div style={{height:"100%",width:"100%",position:"absolute"}}>    
-                        
-                        <CrearReserva handleAddService={this.addService} handleRemoveService={this.removeService} handleCreateReservation={this.createReservation}/>
-                        <Model handleAddService={this.addService} handleRemoveService={this.removeService}/>
-                        
-                    </div>
-                </div>
+            <div className="Servicios" style={{height:"100%",width:"100%",position:"absolute"}}>
+                <ShowReservation handleHideModal={this.hideModal} handleShowModal={this.state.showModal} name={this.state.name} date={this.state.date} time={this.state.time}/>
+                <CrearReserva handleAddService={this.addService} handleRemoveService={this.removeService} handleCreateReservation={this.createReservation}/>
+                <Model handleAddService={this.addService} handleRemoveService={this.removeService}/>                    
             </div>
-         );
+        );
     }
 
     addService = (title, price) =>{
@@ -36,12 +36,20 @@ class Servicios extends Component {
         })});
     }
 
+    hideModal = () => {
+        this.setState({ showModal : "none"})
+    }
+
     createReservation = (name, phone, date, hour, min) => {
         let time = hour + " " + min;
-
+        this.setState({ name : name});
+        this.setState({ date : date});
+        this.setState({ time : time});
+        
         const info = { name: name, phone: phone, day: date, time: time, service: this.state.serviceType }
-                
+        
         if(this.state.serviceType.length > 0){
+            this.setState({ showModal : "initial"});
             this.sendMessage(info);
         }
     }
