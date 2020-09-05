6303225cf9e7e775dee9d127a513a318c752757b
Lautaro Rodriguez <lauta_3011@hotmail.com>
2020-09-05 01:08:43 -0300

    added instagram link on footer

--- client/src/Footer.jsx
+++ client/src/Footer.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Logo from './Logo';
+import igLogo from './imgs/instagram-logo.png';
 
 class Footer extends Component {
     state = {  }
@@ -9,6 +10,7 @@ class Footer extends Component {
                 <div className="Container">
                     <div>
                         <Logo />
+                        <a href="https://www.instagram.com/barbas_rb_/?hl=es-la"><img className="igLogo" src={igLogo} alt="instagram logo"/></a>
                     </div>
                 </div><br/>
                 <h3>DESARROLLO @ <a href="https://lauta3011.github.io/new-portfolio/" style={{color:"white"}}>LAUTARO RODRIGUEZ</a></h3>
