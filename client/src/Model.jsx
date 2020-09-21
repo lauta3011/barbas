438fc0ed68e485eade01446e90befbb2790057df
Lautaro Rodriguez <lauta_3011@hotmail.com>
2020-09-20 21:32:22 -0300

    forgot a components

--- client/src/Model.jsx
+++ client/src/Model.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ListInfo from './ListInfo';
+import ListInfoMobile from './ListInfoMobile'
 import image from './imgs/beardedMen.svg';
 
 class Model extends Component {
@@ -10,12 +11,19 @@ class Model extends Component {
     }
     render() { 
         return ( 
-            <div className="Model">
-                <div className="Container">
-                    <img className="model" src={image} alt="barbudo"/>
-                    <ListInfo isChecked={(title, desc) => this.props.handleAddService(title, desc)} isUnchecked={(title, desc) => this.props.handleRemoveService(title, desc)} listService={this.state.beard} serviceType="Barba"/>
-                    <ListInfo isChecked={(title, desc) => this.props.handleAddService(title, desc)} isUnchecked={(title, desc) => this.props.handleRemoveService(title, desc)}  listService={this.state.hair} serviceType="Pelo"/>
-                    <ListInfo isChecked={(title, desc) => this.props.handleAddService(title, desc)} isUnchecked={(title, desc) => this.props.handleRemoveService(title, desc)}  listService={this.state.eyeBrow}  serviceType="Cejas"/>
+            <div>
+                <div className="Model">
+                    <div className="Container">
+                        <img className="model" src={image} alt="barbudo"/>
+                        <ListInfo isChecked={(title, desc) => this.props.handleAddService(title, desc)} isUnchecked={(title, desc) => this.props.handleRemoveService(title, desc)} listService={this.state.beard} serviceType="Barba"/>
+                        <ListInfo isChecked={(title, desc) => this.props.handleAddService(title, desc)} isUnchecked={(title, desc) => this.props.handleRemoveService(title, desc)}  listService={this.state.hair} serviceType="Pelo"/>
+                        <ListInfo isChecked={(title, desc) => this.props.handleAddService(title, desc)} isUnchecked={(title, desc) => this.props.handleRemoveService(title, desc)}  listService={this.state.eyeBrow}  serviceType="Cejas"/>
+                    </div>
+                </div>
+                <div className="ModelMobile">
+                    <ListInfoMobile isChecked={(title, desc) => this.props.handleAddService(title, desc)} isUnchecked={(title, desc) => this.props.handleRemoveService(title, desc)} listService={this.state.beard} serviceType="Barba"/>
+                    <ListInfoMobile isChecked={(title, desc) => this.props.handleAddService(title, desc)} isUnchecked={(title, desc) => this.props.handleRemoveService(title, desc)}  listService={this.state.hair} serviceType="Pelo"/>
+                    <ListInfoMobile isChecked={(title, desc) => this.props.handleAddService(title, desc)} isUnchecked={(title, desc) => this.props.handleRemoveService(title, desc)}  listService={this.state.eyeBrow}  serviceType="Cejas"/>
                 </div>
             </div>
          );
