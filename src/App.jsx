a007137f0abd81f6b5f2ad42b053e2ba6b3395a4
Lautaro Rodriguez <lauta_3011@hotmail.com>
2020-11-16 21:03:58 -0300

    now i think its really fixec

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ class App extends Component {
                     <Showroom />
                     <Footer />
                 </div>
-                <img src={background} alt="background" style={{width:"100%", height:"120%",position:"fixed",zIndex:"-1",filter:"blur(5px)"}}/>
+                <img src={background} alt="background" style={{width:"100%", height:"100%",position:"fixed",zIndex:"-1",filter:"blur(5px)"}}/>
             </div>
         );
     }
