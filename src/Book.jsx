573fd67056533bc455b2d486f53fad1863eec72c
lautaro <lauta_3011@hotmail.com>
2021-05-26 20:50:42 +0100

    cambio el mandar mails por escribir a wpp

--- src/Book.jsx
+++ src/Book.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import wpp from './imgs/WhatsApp-logo.png' ;
 
 class Book extends Component {
     state = {  }
     render() { 
         return (  
             <div className="Book">
-                <button onClick={this.props.handleReservation} value="RESERVAR">RESERVAR</button>
+                <button onClick={this.props.handleReservation} value="RESERVAR"><img style={{width:'30%', marginLeft:'-50px'}} src={wpp} alt='wpp logo'/>RESERVAR</button>
             </div>
         );
     } 
--- src/Services.jsx
+++ src/Services.jsx
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import CrearReserva from './CrearReserva';
 import Model from './Model';
-import ShowReservation from './ShowReservation';
-import axios from 'axios';
+import axios from 'axios'
 
 class Servicios extends Component {
     state = { 
@@ -18,7 +17,6 @@ class Servicios extends Component {
     render() { 
         return ( 
             <div className="Servicios">
-                <ShowReservation handleSendMessage={this.sendMessage} handleHideModal={this.hideModal} handleShowModal={this.state.showModal} name={this.state.name} date={this.state.date} time={this.state.time}/>
                 <CrearReserva handleAddService={this.addService} handleRemoveService={this.removeService} handleCreateReservation={this.createReservation}/>
                 <Model handleAddService={this.addService} handleRemoveService={this.removeService}/>                    
             </div>
@@ -39,35 +37,38 @@ class Servicios extends Component {
         console.log(title);
     }
 
-    hideModal = () => {
-        this.setState({ showModal : "none"})
-    }
-
     createReservation = (name, phone, date, hour, min) => {
         let time = hour + " " + min;
-        this.setState({ name : name});
-        this.setState({ phone : phone});
-        this.setState({ date : date});
-        this.setState({ time : time});
+        let num = 59895491017;
+        
         
         if(this.state.serviceType.length > 0){
-            this.setState({ showModal : "initial"});
+            let texto = 'Hola! quiero agendarme para el ' + date + ' a la hora ' + time + '. Queria hacerme ';
+            this.state.serviceType.map((s) => {
+                texto += s.title + ' '
+            })
+            texto += '. Soy ' + name + ' y mi telefono es ' + phone;
+
+            console.log(texto);
+            window.location.href = 'https://wa.me/'+ num + '/?text='+ texto;    
         }
     }
 
-    sendMessage = () =>{   
-        const info = { type : 'Reservacion', name: this.state.name, phone: this.state.phone, day: this.state.date, time: this.state.time, service: this.state.serviceType }        
-        const url = 'https://barbas-barbershop.herokuapp.com';
+    //esto quedo deprecado pq ya no quiere usar mail, si no mandar un mensaje por wpp
+    // sendMessage = () =>{   
+        // const info = { type : 'Reservacion', name: this.state.name, phone: this.state.phone, day: this.state.date, time: this.state.time, service: this.state.serviceType }        
+        // const url = 'https://barbas-barbershop.herokuapp.com';
 
-        axios.post(url + '/sendEmail ', info)
-        .then((response)=>{
-            if (response.data.msg === 'success'){
-                console.log('Email sent, awesome');
-            }else if(response.data.msg === 'fail'){
-                console.log("Oops, something went wrong. Try again");
-            }
-        })
-    }
+        // axios.post(url + '/sendEmail ', info)
+        // .then((response)=>{
+        //     if (response.data.msg === 'success'){
+        //         console.log('Email sent, awesome');
+        //     }else if(response.data.msg === 'fail'){
+        //         console.log("Oops, something went wrong. Try again");
+        //     }
+        // })
+
+    // }
 }
  
 export default Servicios;
